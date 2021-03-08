e1805efca17d8d027ce7f8d743efe1d6a185fb8a
astatochek <71458498+astatochek@users.noreply.github.com>
2021-03-09 00:54:48 +0300

    Add files via upload

---
+++ 09.03.2021/bb.bat
@@ -0,0 +1,13 @@
+g++ main.cpp
+a.exe <1_test_in.txt> 1_test_out.txt
+a.exe <2_test_in.txt> 2_test_out.txt
+a.exe <3_test_in.txt> 3_test_out.txt
+a.exe <4_test_in.txt> 4_test_out.txt
+a.exe <5_test_in.txt> 5_test_out.txt
+a.exe <6_test_in.txt> 6_test_out.txt
+a.exe <7_test_in.txt> 7_test_out.txt
+a.exe <8_test_in.txt> 8_test_out.txt
+a.exe <9_test_in.txt> 9_test_out.txt
+a.exe <10_test_in.txt> 10_test_out.txt
+g++ test.cpp
+a.exe
